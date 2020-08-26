46b3369d8ac1c3923c4fe21606954af3cb30ef0b
vishalsripathi <sripathivishalreddy@gmail.com>
2020-08-26 15:54:21 +0530

    created components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupLoginComponent } from './signup-login/signup-login.component';
+import { QuizAppComponent } from './quiz-app/quiz-app.component';
+import { QuizComponent } from './quiz-app/quiz/quiz.component';
+import { ResultComponent } from './quiz-app/result/result.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Login', pathMatch: 'full' },
+  { path: 'Login', component: SignupLoginComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'result', component: ResultComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupLoginComponent } from './signup-login/signup-login.component';
+import { QuizAppComponent } from './quiz-app/quiz-app.component';
+import { NavbarComponent } from './quiz-app/navbar/navbar.component';
+import { QuizComponent } from './quiz-app/quiz/quiz.component';
+import { ResultComponent } from './quiz-app/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupLoginComponent,
+    QuizAppComponent,
+    NavbarComponent,
+    QuizComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
